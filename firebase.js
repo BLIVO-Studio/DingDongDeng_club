018f220e8586c9bcbc1c7720692c6567175fd96d
MECKER714 <114917845+MECKER714@users.noreply.github.com>
2023-02-28 23:38:52 +0900

    1

---
+++ firebase.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCRK7l9YU7CbX7oVFbRyto0uyguFyzQ5Ow",
+    authDomain: "club-sing-list.firebaseapp.com",
+    projectId: "club-sing-list",
+    storageBucket: "club-sing-list.appspot.com",
+    messagingSenderId: "148655864772",
+    appId: "1:148655864772:web:e226bf6a942c2cf4f94291"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+
+
+  
